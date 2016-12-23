2185a8eb98f18456d62e96a407cb215cfa57611a
CorneliaXaos <cornelia.xaos@gmail.com>
2016-12-23 17:41:57 -0500

    Made slight changes to the IStore interface and begun creation of the StaticStore.

--- libxaos-core/interface/memory/store/IStore.h
+++ libxaos-core/interface/memory/store/IStore.h
@@ -1,5 +1,5 @@
-#ifndef   LIBXAOS_MEMORY_STORE_H
-#define   LIBXAOS_MEMORY_STORE_H
+#ifndef     LIBXAOS_MEMORY_STORE_H
+#define     LIBXAOS_MEMORY_STORE_H
 
 #include <cstdint>
 #include <cstdlib>
@@ -16,8 +16,11 @@ namespace libxaos {
          *  a store or not but, generally, this is not a good idea.
          *
          *  To ensure ease of use, a Store should be default constructable.
+         *
+         *  @tparam N the size_t of bytes this Store contains.
+         *  @tparam A the short at which the data should be internally aligned.
          */
-        template<size_t N>
+        template<size_t N, short A>
         class IStore {
             static_assert(N > 0, "A Store must have non-zero storage size!");
 
@@ -32,13 +35,15 @@ namespace libxaos {
                  *  in memory that the using class shall utilize for storing
                  *  information.
                  */
-                virtual uint8_t* getRawStorage() = 0;
+                virtual inline uint8_t* getRawStorage() = 0;
 
-                //! The size, in bytes, which this store contains.
+                //! The size_t, in bytes, which this store contains.
                 static constexpr const size_t SIZE = N;
+                //! The short at which the data should be aligned.
+                static constexpr const short ALIGNMENT = A;
         };
 
     }
 }
 
-#endif // LIBXAOS_MEMORY_STORE_STORE_H
+#endif //   LIBXAOS_MEMORY_STORE_STORE_H
--- libxaos-core/interface/memory/store/impl/StaticStore.h
+++ libxaos-core/interface/memory/store/impl/StaticStore.h
@@ -1,1 +1,53 @@
+#ifndef     LIBXAOS_MEMORY_STATIC_STORE_H
+#define     LIBXAOS_MEMORY_STATIC_STORE_H
 
+#include <cstdint>
+#include <cstdlib>
+#include "memory/store/IStore.h"
+
+namespace libxaos {
+    namespace memory {
+
+        /**
+         *  @brief A StaticStore allocates its data statically.
+         *
+         *  A StaticStore allocates its data in the static section of the
+         *  executable.  This static allocation avoids the troubles of
+         *  dynamically acquiring memory for the store at the expense of being
+         *  fixed at runtime.
+         *
+         *  In order to allow multiple static allocations, a Store is a
+         *  templated class that takes in an additional identification parameter
+         *  which forces the compiler to create an entirely new class for each
+         *  ID.  This allows each class to have its own static array of data.
+         *
+         *  @tparam N the size_t of bytes this store contains.
+         *  @tparam A the short at which the data should be internally aligned.
+         *  @tparam ID the integer uniquely identifying this static allocation.
+         */
+        template<size_t N, short A, int ID> //! @todo reserve negative IDs
+        class StaticStore : public IStore<N, A> {
+
+            public:
+                StaticStore();
+                ~StaticStore();
+
+                //! @todo Copy and Move for the stores? Am not sure...
+
+                //! @see "memory/store/IStore.h"
+                inline uint8_t* getRawStorage() override final;
+
+                //! The StaticStore's unique ID.
+                static constexpr const int STORE_ID = ID;
+
+            private:
+                static uint8_t _store[N]; //!< The Store's static memory.
+        };
+
+    }
+}
+
+// Bring in template definitions.
+#include "StaticStore.tpp.h"
+
+#endif //   LIBXAOS_MEMORY_STATIC_STORE_H
