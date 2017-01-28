641a02cb0b71f0ff2f2a0c5112d449d83b64f04b
CorneliaXaos <cornelia.xaos@gmail.com>
2017-01-27 20:49:10 -0700

    Needed another abstraction layer.  Created Clock.h

---
+++ libxaos-core/interface/timing/Clock.h
@@ -0,0 +1,31 @@
+#ifndef     LIBXAOS_CORE_TIMING_CLOCK_H
+#define     LIBXAOS_CORE_TIMING_CLOCK_H
+
+namespace libxaos {
+    namespace timing {
+
+
+        /**
+         *  @brief Wraps the std::chrono::high_resolution_clock to make it
+         *      easier to use.
+         *
+         *  This namespace wraps the STL implementation of a
+         *  high_resolution_clock on a particular system.
+         *
+         *  Additionally, it wraps the accesses to the clock itself.
+         */
+        namespace Clock {
+
+            //! A wrapper around the high_resolution_clock's duration type.
+            typedef Duration std::chrono::high_resolution_clock::duration;
+            //! A wrapper around the high_resolution_clock's time_point type.
+            typedef TimePoint std::chrono::high_resolution_clock::time_point;
+
+            //! Obtains the current time.
+            inline TimePoint getTime();
+        }
+
+    }
+}
+
+#endif   // LIBXAOS_CORE_TIMING_CLOCK_H
