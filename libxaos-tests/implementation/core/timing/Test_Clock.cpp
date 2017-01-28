bb0e767447c9cd5bc2448b08c662e7a6bd9e087e
CorneliaXaos <cornelia.xaos@gmail.com>
2017-01-27 20:57:28 -0700

    Created Unit Tests for the Clock.

---
+++ libxaos-tests/implementation/core/timing/Test_Clock.cpp
@@ -0,0 +1,32 @@
+/**
+ *  @file Test_Clock.cpp
+ *  @brief Tests: libxaos-core:timing/Clock.h
+ *
+ *  Access the minimized Clock API and tests that it functions correctly.
+ */
+
+#include <chrono>
+#include <thread>
+
+#include "timing/Clock.h"
+
+#include "catch.hpp"
+
+// Define a few... types?
+using Clock = libxaos::timing::Clock;
+
+TEST_CASE("CORE:TIMING/Clock | Can Access the Clock", "[core][timing]") {
+    // This is hard to test.. will just see that it works.
+    volatile Clock::TimePoint time = Clock::getTime();
+}
+
+TEST_CASE("CORE:TIMING/Clock | Clock Actually Represents Time",
+        "[core][timing]") {
+    Clock::TimePoint timeStart = Clock::getTime();
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    Clock::TimePoint timeEnd = Clock::getTime();
+    Clock::Duration duration = timeEnd - start;
+
+    // I highly expect it to be AT LEAST this accurate.
+    REQUIRE(duration > std::chrono::milliseconds(80));
+}
