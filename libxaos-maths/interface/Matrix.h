5dcdc3be08ee4cddd238135be508271c2f7ef2aa
CorneliaXaos <cornelia.xaos@gmail.com>
2017-03-09 21:52:21 -0700

    Added headers for a Matrix class and affine transform namespace.

---
+++ libxaos-maths/interface/Matrix.h
@@ -0,0 +1,114 @@
+#ifndef     LIBXAOS_MATHS_MATRIX_H
+#define     LIBXAOS_MATHS_MATRIX_H
+
+#include <initializer_list>
+#include <type_traits>
+
+#include "Vector.h"
+
+namespace libxaos {
+    namespace maths {
+
+        namespace {
+            template<typename T, int M>
+            using Vector = libxaos::maths::Vector<T, M>;
+        }
+
+        /**
+         *  @brief This class represenets an MxN matrix.
+         *
+         *  This class represents an M column, N row matrix.  Its memory
+         *  is laid out in COLUMN MAJOR order in order to be compatible
+         *  with the OpenGL graphics library.  As such, accessing matrix
+         *  memory with `myMatrix[1][2]` returns the data in the first COLUMN
+         *  and SECOND row.
+         *
+         *  Underneath the hood, the matrix data is actually M number of
+         *  N-length vectors.  This simplifies some of its implementation.
+         *
+         *  This class allows the creation of row or column "matricies" though
+         *  it is recommended that one use Vectors for that unless one wants
+         *  to avoid the "duplicity" that a Vector may be either depending on
+         *  how it is combined with a matrix (left or right multiplication).
+         *
+         *  @tparam T the numeric type of objects contained within the Matrix.
+         *  @tparam M the number of columns in this matrix
+         *  @tparam N the number of rows in this matrix
+         */
+        template<typename T, unsigned int M, unsigned int N>
+        class Matrix {
+            static_assert(std::is_arithmetic<T>::value,
+                    "Matrix types must be arithmetic!");
+            static_assert(M > 0, "Column count must be non-zero!");
+            static_assert(N > 0, "Row count must be non-zero!");
+
+            public:
+                typedef Vector<T, N> ColumnType;
+                typedef Vector<T, M> RowType;
+
+                //! Used for Constructing new Matrices
+                enum InitializationType {
+                    ZERO,
+                    IDENTITY
+                };
+
+                //! Creates a new Matrix based on the Initialization type.
+                Matrix(InitializationType);
+                //! Creates a new Matrix from a nested initializer list
+                explicit Matrix(
+                        std::initializer_list<std::initializer_list<T>>);
+                ~Matrix();
+
+                Matrix(const Matrix<T, M, N>&);
+                Matrix<T, M, N>& operator=(const Matrix<T, M, N>&);
+                Matrix(Matrix<T, M, N>&&);
+                Matrix<T, M, N>& operator=(Matrix<T, M, N>&&);
+
+                //! Accesses a column by reference
+                inline ColumnType& operator[](unsigned int);
+
+                //! Gets a Column of the Matrix
+                inline ColumnType getColumn(unsigned int);
+                //! Gets a Row of the Matrix
+                inline RowType getRow(unsigned int);
+
+            private:
+                ColumnType[M] _data;
+        };
+
+        //! Gets the Transpose of a Matrix
+        template<typename T, unsigned int M, unsigned int N>
+        Matrix<T, M, N> getTranspose(const Matrix<T, M, N>&);
+        //! Transposes the passed in Matrix "in place"
+        template<typename T, unsigned int M, unsigned int N>
+        void transpose(Matrix<T, M, N>&);
+
+        //! Gets the Inverse of a square Matrix (may throw)
+        template<typename T, unsigned int N>
+        Matrix<T, N, N> getInverse(const Matrix<T, N, N>&);
+        //! Inverses a Matrix "in place" (may throw)
+        template<typename T, unsigned int N>
+        void invert(Matrix<T, N, N>&);
+
+        //! Handles Matrix multiplication: (column x row) MxN * OxM = OxN
+        template<typename T, unsigned int M, unsigned int N, unsigned int O>
+        Matrix<T, O, N> operator*(const Matrix<T, M, N>&,
+                const Matrix<T, O, M>&);
+
+        //! Handles (row) Vector by Matrix mupltiplication (vec * mat)
+        template<typename T, unsigned int M, unsigned int N>
+        Vector<T, M> operator*(const Vector<T, N>&, const Matrix<T, M, N>&);
+        //! Handles Matrix by (column) Vector multiplication (mat * vec)
+        template<typename T, unsigned int M, unsigned int N>
+        Vector<T, N> operator*(const Matrix<T, M, N>&, const Vector<T, M>&);
+
+        //! Gets a SubMatrix (or a SuperMatrix filled with 0's) of a Matrix
+        //! Use it like: `getResized<3, 5>(myMat);`
+        template<unsigned int O, unsigned int P, typename T, unsigned int M,
+                unsigned int N>
+        Matrix<T, O, P> getResized(const Matrix<T, M, N>&, unsigned int,
+                unsigned int);
+    }
+}
+
+#endif   // LIBXAOS_MATHS_MATRIX_H
---
+++ libxaos-maths/interface/affine.h
@@ -0,0 +1,78 @@
+#ifndef     LIBXAOS_MATHS_AFFINE_H
+#define     LIBXAOS_MATHS_AFFINE_H
+
+#include "Matrix.h"
+#include "Vector.h"
+
+namespace libxaos {
+    namespace maths {
+
+        /**
+         *  @brief Contains methods for producing 2D and 3D affine transforms.
+         *
+         *  Contains helper methods for producing scale, rotation, and
+         *  translation transformations in two and three dimensions.  All
+         *  matrices are in homogenous coordinates.
+         */
+        namespace affine {
+
+            namespace {
+                template<typename T>
+                using Matrix3 = libxaos::maths::Matrix<T, 3, 3>;
+                template<typename T>
+                using Matrix4 = libxaos::maths::Matrix<T, 4, 4>;
+
+                template<typename T>
+                using Vector2 = libxaos::maths::Vector<T, 2>;
+                template<typename T>
+                using Vector3 = libxaos::maths::Vector<T, 3>;
+            }
+
+            //! 2D Scaling
+            template<typename T>
+            Matrix3<T> get2DScaling(T, T);
+            template<typename T>
+            Matrix3<T> get2DScaling(Vector2<T>);
+            //! 3D Scaling
+            template<typename T>
+            Matrix4<T> get3DScaling(T, T, T);
+            template<typename T>
+            Matrix4<T> get3DScaling(const Vector3<T>&);
+
+            //! 2D Rotation around the only possible axis
+            template<typename T>
+            Matrix3<T> get2DRotation(T);
+            //! 2D Rotation of a Vector2 onto another Vector2
+            template<typename T>
+            Matrix3<T> get2DRotationOnto(const Vector2<T>&, const Vector2<T>&);
+            //! 3D Rotation around coordinate axes: zRot * yRot * xRot = rot
+            template<typename T>
+            Matrix4<T> get3DRotation(T, T, T);
+            template<typename T>
+            Matrix4<T> get3DRotation(const Vector3<T>&);
+            //! 3D Rotation around an arbitrary axis vector
+            template<typename T>
+            Matrix4<T> get3DRotationArbitrary(T, T, T, const Vector3<T>&);
+            template<typename T>
+            Matrix4<T> get3DRotationArbitrary(const Vector3<T>&,
+                    const Vector3<T>&);
+            //! 3D Rotation of a Vector3 onto a Vector3
+            template<typename T>
+            Matrix4<T> get3DRotationOnto(const Vector3<T>&, const Vector3<T>&);
+
+            //! 2D Translation
+            template<typename T>
+            Matrix3<T> get2DTranslation(T, T);
+            template<typename T>
+            Matrix3<T> get2DTranslation(const Vector2<T>&);
+            //! 3D Translation
+            template<typename T>
+            Matrix4<T> get3DTranslation(T, T, T);
+            template<typename T>
+            Matrix4<T> get3DTranslation(const Vector3<T>&);
+
+        }
+    }
+}
+
+#endif   // LIBXAOS_MATHS_AFFINE_H
